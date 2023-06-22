cd4e3ccdfb0fafa74e4029ca161440f05af44291
puneetkathar1 <puneetkathar1@gmail.com>
2023-06-23 02:52:52 +0530

    hehe

--- vite.config.js
+++ vite.config.js
@@ -3,46 +3,6 @@ import postcss from './postcss.config.js'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const manifestForPlugin = {
-  registerType: 'prompt',
-  includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'logo.svg'],
-  manifest: {
-    name: 'TiedUp.app',
-    short_name: 'TiedUp',
-    description: 'For dominants and submissives to track assigned tasks.',
-    icons: [
-      {
-        src: '/pwa-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/pwa-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-        src: '/apple-touch-icon-180x180.png',
-        sizes: '180x180',
-        type: 'image/png',
-        purpose: 'apple touch icon',
-      },
-      {
-        src: '/maskable-icon-512x512.png',
-        sizes: '225x225',
-        type: 'image/png',
-        purpose: 'any maskable',
-      },
-    ],
-    theme_color: '#171717',
-    background_color: '#e8ebf2',
-    display: 'standalone',
-    scope: '/',
-    start_url: '/',
-    orientation: 'portrait',
-  },
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
@@ -53,7 +13,7 @@ export default defineConfig({
   css: {
     postcss,
   },
-  plugins: [react(), VitePWA(manifestForPlugin)],
+  plugins: [react(), VitePWA()],
   resolve: {
     alias: [
       {
