181af8d379eac7d76ad8bc79ad13632414c330b6
puneetkathar1 <puneetkathar1@gmail.com>
2023-06-23 12:28:58 +0530

    hehe

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
 import { Link } from 'react-router-dom'
-// import { useGoogleLogin } from '@react-oauth/google'
+import { useGoogleLogin } from '@react-oauth/google'
 import Header from '../partials/Header'
 import PageIllustration from '../partials/PageIllustration'
 import { useEffect } from 'react'
@@ -29,7 +29,7 @@ function SignIn() {
       setDisabled(true)
       try {
         const response = await fetch(
-          'https://bdsm-backend.onrender.com/api/signin',
+          'https://chatbot-backend-ihn7.onrender.com/api/signin',
           {
             method: 'POST',
             headers: {
@@ -91,7 +91,7 @@ function SignIn() {
     setDisabled(true)
     try {
       const response = await fetch(
-        'https://bdsm-backend.onrender.com/api/signin',
+        'https://chatbot-backend-ihn7.onrender.com/api/signin',
         {
           method: 'POST',
           headers: {
@@ -138,44 +138,12 @@ function SignIn() {
     }
   }
 
-  // const handleSubmitFB = async (data) => {
-  //   setDisabled(true)
-  //   const res = await fetch(`https://bdsm-backend.onrender.com/api/signin`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ fb: data.data }),
-  //   })
-
-  //   const res2 = await res.json()
-  //   console.log(res2)
-  //   if (res2.error) {
-  //     toast({
-  //       title: res2.error,
-  //       status: 'error',
-  //       duration: 9000,
-  //       isClosable: true,
-  //     })
-  //   } else if (res2.token) {
-  //     Cookies.set('token', res2.token, { expires: 7 })
-  //     Cookies.set('email', res2.email, { expires: 7 })
-  //     navigate('/dashboard')
-  //   } else {
-  //     toast({
-  //       title: 'Something went wrong!',
-  //       status: 'error',
-  //       duration: 9000,
-  //       isClosable: true,
-  //     })
-  //   }
-  //   setDisabled(false)
-  // }
-
-  // const googleLogin = useGoogleLogin({
-  //   onSuccess: async (tokenResponse) =>
-  //     handleSubmitGoogle(tokenResponse.access_token),
-  // })
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse)
+      handleSubmitGoogle(tokenResponse.access_token)
+    },
+  })
 
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
@@ -205,7 +173,7 @@ function SignIn() {
               {/* Form */}
               <div className="max-w-sm mx-auto">
                 <div className="flex flex-wrap -mx-3">
-                  {/* <div className="w-full px-3">
+                  <div className="w-full px-3">
                     <button
                       onClick={googleLogin}
                       className="btn px-0 text-white bg-red-600 hover:bg-red-700 w-full relative flex items-center"
@@ -225,44 +193,7 @@ function SignIn() {
                         Sign in with Google
                       </span>
                     </button>
-                  </div> */}
-                  {/* <LoginSocialFacebook
-                    appId="707856684325818"
-                    onResolve={(res) => {
-                      handleSubmitFB(res)
-                    }}
-                    onReject={(err) => console.log(err)}
-                    className="w-full my-2"
-                  >
-                    <FacebookLoginButton />
-                    <div className="w-full px-3">
-                        <button className="btn px-0 text-white bg-blue-700 hover:bg-blue-800 w-full relative flex items-center">
-                          <svg
-                            className="w-4 h-4 fill-current text-white opacity-75 shrink-0 mx-4"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlnsXlink="http://www.w3.org/1999/xlink"
-                            version="1.1"
-                            id="Layer_1"
-                            viewBox="0 0 310 310"
-                            xmlSpace="preserve"
-                          >
-                            <g id="XMLID_834_">
-                              <path
-                                id="XMLID_835_"
-                                d="M81.703,165.106h33.981V305c0,2.762,2.238,5,5,5h57.616c2.762,0,5-2.238,5-5V165.765h39.064   c2.54,0,4.677-1.906,4.967-4.429l5.933-51.502c0.163-1.417-0.286-2.836-1.234-3.899c-0.949-1.064-2.307-1.673-3.732-1.673h-44.996   V71.978c0-9.732,5.24-14.667,15.576-14.667c1.473,0,29.42,0,29.42,0c2.762,0,5-2.239,5-5V5.037c0-2.762-2.238-5-5-5h-40.545   C187.467,0.023,186.832,0,185.896,0c-7.035,0-31.488,1.381-50.804,19.151c-21.402,19.692-18.427,43.27-17.716,47.358v37.752H81.703   c-2.762,0-5,2.238-5,5v50.844C76.703,162.867,78.941,165.106,81.703,165.106z"
-                              />
-                            </g>
-                          </svg>
-                          <span
-                            className="h-6 flex items-center border-r border-white border-opacity-25 mr-4"
-                            aria-hidden="true"
-                          ></span>
-                          <span className="flex-auto pl-16 pr-8 -ml-16">
-                            Sign in with Facebook
-                          </span>
-                        </button>
-                      </div>
-                  </LoginSocialFacebook> */}
+                  </div>
                 </div>
                 <div className="flex items-center my-6">
                   <div
@@ -280,7 +211,7 @@ function SignIn() {
                 <div className="flex flex-wrap -mx-3 mb-4">
                   <div className="w-full px-3">
                     <label
-                      className="block  text-sm font-medium mb-1"
+                      className="block text-gray-300 text-sm font-medium mb-1"
                       htmlFor="email"
                     >
                       Email
@@ -288,7 +219,7 @@ function SignIn() {
                     <input
                       id="email"
                       type="email"
-                      className="form-input w-full "
+                      className="form-input w-full text-gray-300"
                       placeholder="you@yourcompany.com"
                       required
                       value={email}
@@ -299,7 +230,7 @@ function SignIn() {
                 <div className="flex flex-wrap -mx-3 mb-4">
                   <div className="w-full px-3">
                     <label
-                      className="block  text-sm font-medium mb-1"
+                      className="block text-gray-300 text-sm font-medium mb-1"
                       htmlFor="password"
                     >
                       Password
@@ -307,7 +238,7 @@ function SignIn() {
                     <input
                       id="password"
                       type="password"
-                      className="form-input w-full "
+                      className="form-input w-full text-gray-300"
                       placeholder="Password (at least 10 characters)"
                       required
                       value={password}
