8cd88fd1a625b4dc888f223ab57f150776e1d412
puneetkathar1 <puneetkathar1@gmail.com>
2023-08-07 21:19:13 +0530

    Your commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -1,59 +1,77 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import App from './App';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import App from './App'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const theme = extendTheme({
   colors: {
     brand: '#5D5DFF',
     white: '#fff',
   },
-});
+})
 
 const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register('/sw.js');
-      console.log('Service worker registered:', registration);
+      const registration = await navigator.serviceWorker.register('/sw.js')
+      console.log('Service worker registered:', registration)
     } catch (err) {
-      console.error('Failed to register service worker:', err);
+      console.error('Failed to register service worker:', err)
     }
   }
-};
+}
 
 const requestNotificationPermission = async () => {
   try {
-    const permissionResult = await Notification.requestPermission();
-    return permissionResult === 'granted';
+    const permissionResult = await Notification.requestPermission()
+    return permissionResult === 'granted'
   } catch (err) {
-    console.error('Error requesting notification permission:', err);
-    return false;
+    console.error('Error requesting notification permission:', err)
+    return false
   }
-};
+}
 
 const subscribeToPush = async () => {
   if (!('PushManager' in window)) {
-    console.error('Push notifications are not supported in this browser');
-    return;
+    console.error('Push notifications are not supported in this browser')
+    return
   }
 
   try {
-    const registration = await navigator.serviceWorker.ready;
-    const subscription = await registration.pushManager.subscribe({
+    const registration = await navigator.serviceWorker.ready
+
+    // Replace 'YOUR_PUBLIC_KEY' with the actual VAPID public key you generated
+    const applicationServerKey = 'BJGSc-NAWBwkw5pXl_FaUNj2ebiXtPJm_qRUfsTkMCps6jSBm67xqn12FBpH53woCjmp6mjRZel2uhZXOhdgQyY'
+
+    const options = {
       userVisibleOnly: true,
-    });
+      applicationServerKey: urlBase64ToUint8Array(applicationServerKey),
+    }
+
+    const subscription = await registration.pushManager.subscribe(options)
 
     // Send the subscription data to your backend (via an API call, for example)
-    await sendSubscriptionToBackend(subscription);
+    await sendSubscriptionToBackend(subscription)
 
-    console.log('Push subscription successful');
+    console.log('Push subscription successful')
   } catch (err) {
-    console.error('Error subscribing to push notifications:', err);
+    console.error('Error subscribing to push notifications:', err)
   }
-};
+}
 
+// Helper function to convert base64 string to Uint8Array
+function urlBase64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
 const sendSubscriptionToBackend = async (subscription) => {
   // Send the subscription data to your backend
   // You need to implement this API call to send the subscription to your backend
@@ -64,22 +82,22 @@ const sendSubscriptionToBackend = async (subscription) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(subscription),
-  });
-};
+  })
+}
 
 const Main = () => {
   useEffect(() => {
-    registerServiceWorker();
+    registerServiceWorker()
     const initializePushNotifications = async () => {
-      const permissionGranted = await requestNotificationPermission();
+      const permissionGranted = await requestNotificationPermission()
       if (permissionGranted) {
-        await subscribeToPush();
+        await subscribeToPush()
       } else {
-        console.log('Permission denied for push notifications');
+        console.log('Permission denied for push notifications')
       }
-    };
-    initializePushNotifications();
-  }, []);
+    }
+    initializePushNotifications()
+  }, [])
 
   return (
     <React.StrictMode>
@@ -91,7 +109,7 @@ const Main = () => {
         </GoogleOAuthProvider>
       </ChakraProvider>
     </React.StrictMode>
-  );
-};
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Main />);
+ReactDOM.createRoot(document.getElementById('root')).render(<Main />)
